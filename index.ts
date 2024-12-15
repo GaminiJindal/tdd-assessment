9e4e1820765b92342085ef599bb4a69f94ab1963
Gamini Jindal <gaminijindal@gmail.con>
2024-12-15 13:20:42 +0530

    initial code created
    TESTED WITH Input: “”, Output: 0
    Input: “1”, Output: 1
    Input: “1,5”, Output: 6
    
    WORKING FINE

---
+++ index.ts
@@ -0,0 +1,20 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+ function add( numbers:string){
+    let sum=0
+   
+   for(let char of numbers){
+       console.log(char,"egdngdnb")
+       if(!isNaN(parseInt(char))){
+           sum=sum+parseInt(char)
+       }
+       
+   }
+   return sum
+}
+
+
+const result=add("1,7,8,9,67")
+
+console.log(result,"resultresult")
